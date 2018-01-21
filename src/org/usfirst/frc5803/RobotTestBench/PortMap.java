e35b958f5218b97ad5540db64ef5b5089c841fdb
Apex5803 <steph@apexfrc.com>
2018-01-20 18:08:51 -0800

    It might be ready to go on the robot. It might not. we dont really know.
    We unlocked the most advanced programming technique - shamelessly rip off other teams.

---
+++ src/org/usfirst/frc5803/RobotTestBench/PortMap.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc5803.RobotTestBench;
+
+public class PortMap {
+
+	public static final int PORT_0=0;
+	public static final int PORT_1=1;
+	public static final int PORT_3=2;
+	public static final int PORT_4=4;
+	public static final int PORT_5=5;
+	public static final int PORT_6=6;
+	public static final int PORT_7=7;
+	public static final int PORT_8=8;
+	public static final int PORT_9=9;
+	public static final int PORT_10=10;
+	public static final int PORT_11=11;
+	public static final int PORT_12=12;
+	public static final int PORT_13=13;
+	public static final int PORT_14=14;
+	public static final int PORT_15=15;
+}
